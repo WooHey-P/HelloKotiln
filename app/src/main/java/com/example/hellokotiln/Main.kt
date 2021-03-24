1864eee2b726e7dfb3fabd8f330d461174ef2a6b
WooHyun-P <dkqnwlnd@naver.com>
2021-03-24 22:09:05 +0900

    if, for-loop, when

--- app/src/main/java/com/example/hellokotiln/Main.kt
+++ app/src/main/java/com/example/hellokotiln/Main.kt
@@ -1,18 +1,27 @@
 package com.example.hellokotiln
 
 fun main() {
-    // 코틀린은 자바의 에러를 줄이기 위해 만든 것.
-    // 따라서 특별한 경우가 아니라면 변수 var보다 상수 val로 선언
+    // 코틀린 만의 특별한
 
-    // 배열
-    val _arrayitems = arrayOf(1, 2, 3, 4, 5)
-    // 리스트  원소 추가 등 변경 불가
-    val _listitems = listOf(1, 2, 3, 4, 5)
-    
-    print("$_arrayitems\n")      // [Ljava.lang.Integer;@4617c264
-    print("$_listitems \n")     // [1, 2, 3, 4, 5]
+    // if 식 (if 문)
+    val x = 2;
+    val isEven = if (x % 2 == 0) "짝수" else "홀수"
 
-    // arrayList 원소 추가 등 변경 가능
-    val _arrlist = arrayListOf(1, 2, 3)
-    
+    // for-loop
+    for (i in 0..9) {
+        // 0 부터 9 까지 출력
+        println(i)
+    }
+
+    // switch문 코틀린에서는 when 문
+    val y = 1
+    // switch 문에 대한 결과값을 상수에 대입 가능
+    val ss = when (x) {
+        1-> println("1")
+        2-> println("2")
+        3, 4, 5 -> println("3, 4, 5임")
+        in 6..20 -> print("6부터 20 사이임")             // in 을 사용해서 해당 범위에 속한다면 실행
+        !in 8..10 -> println("8부터 10이 아님")
+        else -> print("default")
+    }
 }
