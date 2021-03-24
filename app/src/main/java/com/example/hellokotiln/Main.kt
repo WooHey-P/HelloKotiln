d851ebd95c7cdf5ece48410a1b718cb4e0b63938
WooHyun-P <dkqnwlnd@naver.com>
2021-03-24 21:36:49 +0900

    array, list, arrayList 자료구조

--- app/src/main/java/com/example/hellokotiln/Main.kt
+++ app/src/main/java/com/example/hellokotiln/Main.kt
@@ -1,20 +1,18 @@
 package com.example.hellokotiln
 
 fun main() {
-    var x1 = 10          // 타입 미지정 변수
-    var x2: Int = 10     // int형 변수 (변경 가능)
-    var isMarried = true        // 선언과 동시에 값 초기화하기
+    // 코틀린은 자바의 에러를 줄이기 위해 만든 것.
+    // 따라서 특별한 경우가 아니라면 변수 var보다 상수 val로 선언
 
-    var a: Int = 10
-    // int, float, double ... 등등은 kotlin에서는 첫 문자가 대문자로 바뀜
+    // 배열
+    val _arrayitems = arrayOf(1, 2, 3, 4, 5)
+    // 리스트  원소 추가 등 변경 불가
+    val _listitems = listOf(1, 2, 3, 4, 5)
+    
+    print("$_arrayitems\n")      // [Ljava.lang.Integer;@4617c264
+    print("$_listitems \n")     // [1, 2, 3, 4, 5]
 
-    val b = 10      // 상수 const (변경 불가)
-//    b = 20        // 불가
-
-    var s = "hello"
-    print("$s 월드")      // 변수s를 넣음
-
-    print(myfun_return(10, 20))
+    // arrayList 원소 추가 등 변경 가능
+    val _arrlist = arrayListOf(1, 2, 3)
+    
 }
-
-fun myfun_return(a: Int, b: Int): Int = a + b;
