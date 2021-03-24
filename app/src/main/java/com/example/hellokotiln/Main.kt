83d01d9ad72169263f691de69d285fc659de356b
WooHyun-P <dkqnwlnd@naver.com>
2021-03-24 21:17:27 +0900

    코틀린 fun method, return type 등

--- app/src/main/java/com/example/hellokotiln/Main.kt
+++ app/src/main/java/com/example/hellokotiln/Main.kt
@@ -1,5 +1,20 @@
 package com.example.hellokotiln
 
 fun main() {
-    print("Hello World")        // 콘솔에 출력
+    var x1 = 10          // 타입 미지정 변수
+    var x2: Int = 10     // int형 변수 (변경 가능)
+    var isMarried = true        // 선언과 동시에 값 초기화하기
+
+    var a: Int = 10
+    // int, float, double ... 등등은 kotlin에서는 첫 문자가 대문자로 바뀜
+
+    val b = 10      // 상수 const (변경 불가)
+//    b = 20        // 불가
+
+    var s = "hello"
+    print("$s 월드")      // 변수s를 넣음
+
+    print(myfun_return(10, 20))
 }
+
+fun myfun_return(a: Int, b: Int): Int = a + b;
